b7d58db93e6ede00974cbd606f24f7f597484bc1
ben <ben.mcinally@btinernet.com>
2024-11-15 22:09:15 +0000

    adds tittles to pages, removes root layout

--- test/sample_app_web/controllers/static_page_controller_test.exs
+++ test/sample_app_web/controllers/static_page_controller_test.exs
@@ -4,16 +4,22 @@ defmodule SampleAppWeb.StaticPageControllerTest do
   # Note the pattern matching to get conn
   test "should get home", %{conn: conn} do
     conn = get(conn, ~p"/static_pages/home")
-    assert html_response(conn, 200)
+
+    html_response(conn, 200)
+    |> assert_select("title", "Home | Phoenix Tutorial Sample App")
   end
 
   test "should get help", %{conn: conn} do
     conn = get(conn, ~p"/static_pages/help")
-    assert html_response(conn, 200)
+
+    html_response(conn, 200)
+    |> assert_select("title", "Help | Phoenix Tutorial Sample App")
   end
 
   test "should get about", %{conn: conn} do
     conn = get(conn, ~p"/static_pages/about")
-    assert html_response(conn, 200)
+
+    html_response(conn, 200)
+    |> assert_select("title", "About | Phoenix Tutorial Sample App")
   end
 end
